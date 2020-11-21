55018bfa5e537679ae63da0fa651db4e941ec89a
Rayan-Tarapore <ratitarapore@gmail.com>
2020-11-21 19:48:27 +0530

    I KILLED THE PIGS

--- Slingshot.js
+++ Slingshot.js
@@ -17,7 +17,9 @@ class SlingShot{
     fly(){
         this.sling.bodyA = null;
     }
-
+    attach(bodyA){
+            this.sling.bodyA = bodyA;
+    }
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
